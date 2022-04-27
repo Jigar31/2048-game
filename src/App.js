d8a81a882028f9e2e4de39a12cfccb3eb863d899
Jigar Punjani <jigarpunjani@gmail.com>
2022-04-27 10:50:27 +0530

    for-netlify

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.scss";
 import Board from "./components/Board/Board";
 
 function App() {
-  const size = parseInt(process.env.REACT_APP_GRID_SIZE);
-  const winningNumber = parseInt(process.env.REACT_APP_WINNING_NUMBER);
+  const size = parseInt(process.env.REACT_APP_CUSTOM_ENV_VAR_GRID_SIZE);
+  const winningNumber = parseInt(
+    process.env.REACT_APP_CUSTOM_ENV_VAR_WINNING_NUMBER
+  );
 
   return (
     <div className="app">
