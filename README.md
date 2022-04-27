09add998ba9089051b3ef590c3b9c2931b2e0685
Jigar Punjani <jigarpunjani@gmail.com>
2022-04-27 21:51:38 +0530

    main

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 To run: 
 1) npm i
 2) npm start
+
+
+Add below property in .env:
+
+REACT_APP_GRID_SIZE=5 => for grid size
+REACT_APP_WINNING_NUMBER=2048 => for winning tile
--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.scss";
 import Board from "./components/Board/Board";
 
 function App() {
-  const size = parseInt(process.env.REACT_APP_GRID_SIZE);
-  const winningNumber = parseInt(process.env.REACT_APP_WINNING_NUMBER);
+  const size = parseInt(process.env.REACT_APP_GRID_SIZE || 4);
+  const winningNumber = parseInt(process.env.REACT_APP_WINNING_NUMBER || 2048);
 
   return (
     <div className="app">
